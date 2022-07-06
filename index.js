cc3e4690692213357a103e0446c48b9a55736434
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2022-07-06 16:24:48 -0300

    Add rota get /products

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const app = require('./app');
 require('dotenv').config();
+const bodyParser = require('body-parser');
+const storeControllers = require('./controllers/storeController');
+app.use(bodyParser.json());
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
 });
+
+app.get('/products', storeControllers.productController);
+
