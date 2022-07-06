7fa965ad9619f274af1cfb0fab83f40705041b49
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2022-07-06 16:25:51 -0300

    Cria getAllProdById controller

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -5,7 +5,16 @@ const productController = async (_req, res) => {
   return res.status(200).json(response);
 };
 
+const productByIdController = async (req, res) => {
+  const { id } = req.params;
+  const response = await storeServices.productFilterById(id);
+  if (!response) {
+    return res.status(404).json({ message: 'Product not found' });
+  }
+  return res.status(200).json(response);
+};
 
 module.exports = {
   productController,
+  productByIdController,
 };
