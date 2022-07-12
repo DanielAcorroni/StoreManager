841b4ee5512a81972db41ac34d910c711bc6ab5a
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2022-07-12 14:57:14 -0300

    Cria updateProdController

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -43,6 +43,16 @@ const createSaleController = async (req, res) => {
   return res.status(201).json(createStat);
 };
 
+const updateProdController = async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const updateStat = await storeServices.updateProdService({ id, name });
+  if (!updateStat) {
+    return res.status(404).json({ message: 'Product not found' });
+  }
+  return res.status(200).json(updateStat);
+};
+
 module.exports = {
   productController,
   productByIdController,
@@ -50,4 +60,5 @@ module.exports = {
   createSaleController,
   salesController,
   salesByIdController,
+  updateProdController,
 };
